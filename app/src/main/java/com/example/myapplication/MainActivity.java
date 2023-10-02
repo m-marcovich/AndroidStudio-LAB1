e67d01d3483cadeea779f0707852cfd6512b756b
Maksym Mikhieiev <mih123eeev@gmail.com>
2023-10-02 23:14:53 +0300

    Added change text functionality

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,20 +2,27 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView tvMain;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.tvMain = findViewById(R.id.textView);
     }
     public void ChangeTexT(View view)
     {
-        Toast.makeText( this, "Some message", Toast.LENGTH_LONG).show();
+    this.tvMain.setText("Change Text");
     }
 
 }
